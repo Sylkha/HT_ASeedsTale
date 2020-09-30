ce90142db5e431bff866a55545fa4fb5c26cfcb6
Silvia Osoro <silwia.o.g@gmail.com>
2020-09-30 11:36:41 +0200

    Empezando pruebas cambio de estación
    Una nueva escena y planteamiento para hacer el cambio de estación poco a poco.

---
+++ Assets/MyProject/Scripts/Missions/SeasonsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonsManager : MonoBehaviour
+{
+    // Physics.IgnoreLayerCollision (layer1, layer2, true) Physics.IgnoreLayerCollision (layer1, layer2, false)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
