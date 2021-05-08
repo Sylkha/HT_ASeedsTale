032ce89eb892742d2a790d6a12392c57b5a564cd
Silvia Osoro <silwia.o.g@gmail.com>
2021-05-08 21:33:50 +0200

    0.7.3
    Metido el Audio Settings en el menú. (A falta de probarlo y dejar abiertas ciertas variables)

---
+++ Assets/MyProject/Scripts/Menus/MyAudioSettings.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://scottgamesounds.com/wp-content/uploads/2018/12/C.AudioSettings.txt
+public class MyAudioSettings : MonoBehaviour
+{
+    FMOD.Studio.EventInstance SFXVolumeTestEvent;
+
+    FMOD.Studio.Bus Music;
+    FMOD.Studio.Bus SFX;
+    FMOD.Studio.Bus Master;
+    float MusicVolume = 0.5f;
+    float SFXVolume = 0.5f;
+    float MasterVolume = 1f;
+
+    void Awake()
+    {
+        Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+        SFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
+        Master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
+        SFXVolumeTestEvent = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/SFXVolumeTest");
+    }
+
+    private void Start()
+    {
+        MasterVolume = PlayerPrefs.GetFloat("masterVolume", 1f);
+        MusicVolume = PlayerPrefs.GetFloat("musicVolume", 0.5f);
+        SFXVolume = PlayerPrefs.GetFloat("sfxVolume", 0.5f);
+    }
+
+    void Update()
+    {
+        Master.setVolume(MasterVolume);
+        Music.setVolume(MusicVolume);
+        SFX.setVolume(SFXVolume);
+    }
+
+    public void MasterVolumeLevel(float newMasterVolume)
+    {
+        MasterVolume = newMasterVolume;
+        PlayerPrefs.SetFloat("masterVolume", newMasterVolume);
+    }
+
+    public void MusicVolumeLevel(float newMusicVolume)
+    {
+        MusicVolume = newMusicVolume;
+        PlayerPrefs.SetFloat("musicVolume", newMusicVolume);
+    }
+
+    public void SFXVolumeLevel(float newSFXVolume)
+    {
+        SFXVolume = newSFXVolume;
+        PlayerPrefs.SetFloat("sfxVolume", newSFXVolume);
+
+        FMOD.Studio.PLAYBACK_STATE PbState;
+        SFXVolumeTestEvent.getPlaybackState(out PbState);
+        if (PbState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            SFXVolumeTestEvent.start();
+        }
+    }
+}
